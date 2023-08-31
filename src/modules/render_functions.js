afd1ea9593b56414385193a1e2510c060f5ebd4c
Afimia Fidelis <38649892+Izebiafe@users.noreply.github.com>
2023-08-31 22:06:43 +0000

    Add leaderboard Api

--- src/modules/render_functions.js
+++ src/modules/render_functions.js
@@ -1,38 +1,38 @@
-const appendLeaderboardUl = (name, score) => {
+import { loadLeaderBoard, postNewScore } from './api_manager.js';
+
+const populateLeaderBoard = (leaders) => {
   const ulLeaderboard = document.getElementById('leaderboard');
-  const liLeaders = document.createElement('li');
-  liLeaders.className = 'leaders';
-  liLeaders.textContent = `${name}: ${score}`;
-  ulLeaderboard.appendChild(liLeaders);
+  ulLeaderboard.innerHTML = '';
+  leaders.forEach((leader) => {
+    const liLeaders = document.createElement('li');
+    liLeaders.className = 'leaders';
+    liLeaders.textContent = `${leader.user}: ${leader.score}`;
+    ulLeaderboard.appendChild(liLeaders);
+  });
 };
 
-const leaders = [
-  {
-    name: 'John Doe',
-    score: 85,
-  },
-  {
-    name: 'Cameron Brown',
-    score: 76,
-  },
-  {
-    name: 'Folden Smith',
-    score: 50,
-  },
-  {
-    name: 'Boston Doe',
-    score: 65,
-  },
-  {
-    name: 'Camila Brown',
-    score: 25,
-  },
-];
+const formAddNewSubmit = (e) => {
+  e.preventDefault();
+  const scoreData = {
+    user: document.getElementById('username').value,
+    score: document.getElementById('score').value,
+  };
+  postNewScore(scoreData);
+  document.getElementById('form-add-new').reset();
+  e.stopPropagation();
+};
 
-const renderUI = () => {
-  leaders.forEach((leader) => {
-    appendLeaderboardUl(leader.name, leader.score);
-  });
+const btnRefreshClick = (e) => {
+  loadLeaderBoard(populateLeaderBoard);
+  e.stopPropagation();
+};
+
+const renderUI = async () => {
+  loadLeaderBoard(populateLeaderBoard);
+  const btnRefresh = document.getElementById('btn-refresh');
+  const formAddNew = document.getElementById('form-add-new');
+  btnRefresh.addEventListener('click', btnRefreshClick);
+  formAddNew.addEventListener('submit', formAddNewSubmit);
 };
 
 export default renderUI;
